f67726c0ac4b35c89c3171995230c67ec3681192
Amit Mahato <amit643.mahato@gmail.com>
2022-12-18 02:46:35 +0545

    feat: update client & server side code with required logic - logic to generate proof and verify proof for checking eligibility of a person to get the gift

--- client/index.js
+++ client/index.js
@@ -1,16 +1,26 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const randomIndex = Math.floor(Math.random() * niceList.length);
+  const randomPerson = niceList[randomIndex];
+
+  const merkleTree = new MerkleTree(niceList);
+  // how do we prove to the server we're on the nice list?
+  // by generating a proof based on the merkle tree
+  const proof = merkleTree.getProof(randomIndex);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof,
+    name: randomPerson,
   });
 
+  console.log(
+    `Hey, ${randomPerson} at index ${randomIndex}. Let us find if you can get a gift.`
+  );
   console.log({ gift });
 }
 
--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
+// hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "f38b1bbe9b84eac10b519b0700061fbb716136fad75ba1f03e7597079d0752d5";
 
-app.post('/gift', (req, res) => {
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  //  prove that a name is in the list
+  const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT);
+  if (isInTheList) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
